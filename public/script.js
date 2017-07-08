e53c0f094f177dde768cfaeeda36b89d1728d2f2
Teodor Lagerqvist <teodor@pingpong.net>
2017-07-08 19:48:19 +0200

    add service worker

--- public/script.js
+++ public/script.js
@@ -28,11 +28,79 @@ fetch("/api/departures/9191")
 			.join("");
 	});
 
-
 function printDeparture(d) {
 	return `<div>
 		<span class="line-number">${d.LineNumber}</span>
 		<span class="destination">${d.Destination}</span>
 		<span>${d.DisplayTime}</span>
 	</div>`;
+}
+
+
+
+
+
+
+
+// Service Worker stuff
+if ('serviceWorker' in navigator) {
+	console.log("register service worker");
+	navigator.serviceWorker.register('/sw.js')
+		.then(function(registration) {
+			console.log(`service worker registered`);
+
+			// no service worker active, first visit
+			if (!navigator.serviceWorker.controller) {
+				console.log("first visit");
+				//return;
+			}
+
+			if (registration.waiting) {
+				console.log("new worker ready");
+				updateReady(registration.waiting);
+				return;
+			}
+
+			if (registration.installing) {
+				console.log("new worker installing");
+				trackInstalling(registration.installing);
+				return;
+			}
+
+			registration.addEventListener('updatefound', function(e) {
+				console.log("new worker found, track installation", e);
+				trackInstalling(registration.installing);
+			});
+		})
+		.catch(function(err) {
+			console.log("Service worker registration failed : ", err);
+		})
+	;
+
+	var refreshing;
+	navigator.serviceWorker.addEventListener('controllerchange', function() {
+		if (refreshing) return;
+		console.log("Refresh page!");
+		//window.location.reload();
+		refreshing = true;
+	});
+
+	var trackInstalling = function(worker) {
+		var indexController = this;
+		worker.addEventListener('statechange', function() {
+			console.log("STATE CHANGE", worker.state);
+			if (worker.state == 'installed') {
+				updateReady(worker);
+			}
+		});
+	};
+
+	var updateReady = function(worker) {
+		console.log("Update ready!");
+		worker.postMessage({action: 'skipWaiting'});
+	};
+
+	navigator.serviceWorker.onmessage = function(e) {
+		console.log("Message form SW:", e.data);
+	}
 }
---
+++ public/sw.js
@@ -0,0 +1,66 @@
+const cacheName = '1.1.3';
+
+const cachedFiles = [
+	'/',
+	'manifest.json',
+	'script.js',
+	'style.css'
+];
+
+self.addEventListener('activate', e => {
+	console.log("SW: Activate new service worker.", cacheName);
+
+	e.waitUntil(
+		caches.keys().then(keyList => {
+			return Promise.all(keyList.map(key => {
+				if(key !== cacheName) {
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
+});
+
+
+self.addEventListener('message', e => {
+	if (e.data.action === 'skipWaiting') {
+		self.skipWaiting();
+	}
+});
+
+self.addEventListener('install', e => {
+	console.log("SW: Installing service worker");
+	e.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll(cachedFiles).then(_ => {
+				console.log("SW: Installation complete, files saved to cache", cachedFiles);
+				//self.skipWaiting();
+			});
+		})
+	);
+});
+
+self.addEventListener('fetch', e => {
+	//console.log("SW: fetcha", e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(response => {
+			if(response) {
+				console.log("SW: get from cache", response.url);
+				return response;
+			} else {
+				console.log("SW: get from server", e.request.url);
+				return fetch(e.request);
+			}
+		})
+	);
+});
+
+
+self.addEventListener('push', e => {
+	console.log("Push", e);
+	e.waitUntil(
+		self.registration.showNotification('ServiceWorker Cookbook', {
+			body: 'Alea iacta est',
+		})
+	);
+});
